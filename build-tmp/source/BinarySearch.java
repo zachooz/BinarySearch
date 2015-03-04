5384684e61c95c833e7e4daa34999705bdf6bc2c
Zachary Hoffman <zachary.j.hoffman@gmail.com>
2015-03-03 20:12:04 -0800

    finished all searches

--- build-tmp/source/BinarySearch.java
+++ build-tmp/source/BinarySearch.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class BinarySearch extends PApplet {
+
 private Item[] store = {new Item(184,14),
         new Item(196,60),
         new Item(206,31),
@@ -111,3 +127,12 @@ public void draw()
 
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "BinarySearch" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
